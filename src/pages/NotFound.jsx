f6b22f359e41c38352fc2fca1f19257c2f1879bf
Jesús Borrero <39781228+jesubohr@users.noreply.github.com>
2022-06-14 02:59:09 -0500

    NotFound page styled

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,14 @@
+import Section from "@comp/Section"
+import Title from "@comp/Title"
+import Button from "@comp/Button"
+
 export default function NotFound () {
   return (
-    <section>
-      <h1>404</h1>
-    </section>
+    <Section>
+      <Title>404</Title>
+      <Title.Sub>Page not found</Title.Sub>
+      <img className="my-8" src="https://img.icons8.com/pastel-glyph/128/272343/page-not-found--v2.png"/>
+      <Button to="/">Go to home</Button>
+    </Section>
   )
 }
