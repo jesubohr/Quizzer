39ab2a3e67876b4abc698c479901669972a12b9c
Jesús Borrero <39781228+jesubohr@users.noreply.github.com>
2022-06-14 03:32:14 -0500

    Delete history functionality

--- src/components/History.jsx
+++ src/components/History.jsx
@@ -1,13 +1,24 @@
-export default function History ({ history }) {
+export default function History ({ history, setHistory }) {
+
+  const handleRemove = (index) => {
+    setHistory(history.filter((_, i) => i !== index))
+  }
+
   return (
-    <ul className="flex justify-center flex-wrap gap-5 my-5">
+    <ul className="flex justify-center flex-wrap gap-5 mt-7 mb-5">
       {
-        history.map(({ name, score }, index) => (
-          <li key={ index } className="py-2 px-4 bg-secondary border-2 border-tertiary rounded text-paragraph text-center text-lg">
-            <p className="text-xl font-medium">{ name }</p>
-            <p className="">{ score } points</p>
-          </li>
-        ))
+        history
+          .sort((a, b) => a.score - b.score)
+          .map(({ name, score }, index) => (
+            <li key={ index } className="relative py-2 px-4 bg-secondary border-2 border-tertiary rounded text-paragraph text-center text-lg">
+              <p className="text-xl font-medium">{ name }</p>
+              <p>{ score } points</p>
+              <button
+                className="absolute -top-3.5 -right-3.5 w-7 h-7 bg-white border-2 border-tertiary rounded-full text-base"
+                onClick={ () => handleRemove(index) }
+              >✖</button>
+            </li>
+          ))
       }
     </ul>
   )
--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -25,14 +25,14 @@ export default function Result () {
         <Title.Sub>
           {
             userScore > 0
-              ? `You have scored ${ userScore } points`
+              ? `You have scored ${userScore} points`
               : `You have 0 points`
           }
         </Title.Sub>
       </Section.Sub>
       <Section.Sub className="mt-10">
         <Title.Sub className="text-2xl font-semibold">History</Title.Sub>
-        <History history={ userHistory } />
+        <History history={ userHistory } setHistory={ setUserHistory } />
       </Section.Sub>
       <Button to="/">
         Start again
